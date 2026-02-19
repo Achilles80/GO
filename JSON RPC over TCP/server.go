466550355ceeadc70b2f150cadb482b303489b5e
Achilles80 <adithyaajay2005@gmail.com>
2026-02-19 18:09:35 +0530

    JSON RPC over TCP server

---
+++ JSON RPC over TCP/server.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+type Args struct{
+	A int 
+	B int 
+}
+type Arith struct{}
+func (a *Arith) Add(args *Args,reply *int) error {
+	*reply=args.A+args.B
+	return nil
+}
+func main(){
+	if err:=rpc.Register(new(Arith)); err!=nil{
+		log.Fatal(err)
+	}
+	ln,err:=net.Listen("tcp",":1234")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	log.Println("JSON RPC Server on :1234")
+	for{
+		conn,err:=ln.Accept()
+		if err!=nil{
+			log.Fatal(err)
+		}
+		go jsonrpc.ServeConn(conn)
+	}
+}
