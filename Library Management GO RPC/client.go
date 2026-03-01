32799b4e4b6168e67772f5f154f449efd08a4c7a
Achilles80 <adithyaajay2005@gmail.com>
2026-03-01 12:42:48 +0530

    Added terminal input for Library client GO

--- Library Management GO RPC/client.go
+++ Library Management GO RPC/client.go
@@ -3,6 +3,9 @@ import (
 	"log"
 	"fmt"
 	"net/rpc"
+	"bufio"
+	"os"
+	"strings"
 )
 type Book struct{
 	BookID int
@@ -18,12 +21,19 @@ func main(){
 		log.Fatal(err)
 	}
 	defer client.Close()
-	newBook:=&Book{BookID:101,Title:"Hello World"}
+	reader:=bufio.NewReader(os.Stdin)
+	fmt.Println("Enter book id: ")
+	var bookid int 
+	fmt.Scanln(&bookid)
+	fmt.Println("Enter book title: ")
+	title,_:=reader.ReadString('\n')
+	title=strings.TrimSpace(title)
+	newBook:=&Book{BookID:bookid,Title:title}
 	var msgReply string 
 	var boolReply bool 
 	client.Call("Library.Add",newBook,&msgReply)
 	fmt.Println("Add: ",msgReply)
-	args:=&BookArgs{BookID:101}
+	args:=&BookArgs{BookID:bookid}
 	client.Call("Library.CheckAvailability",args,&boolReply)
 	fmt.Println("CheckAvailability: ",boolReply)
 	client.Call("Library.Issue",args,&msgReply)
