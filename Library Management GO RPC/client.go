30a3ba1b1349ddce8962151ae90a65ee1b47375c
Achilles80 <adithyaajay2005@gmail.com>
2026-03-01 13:14:58 +0530

    Terminal input for library GO RPC

--- Library Management GO RPC/client.go
+++ Library Management GO RPC/client.go
@@ -21,27 +21,49 @@ func main(){
 		log.Fatal(err)
 	}
 	defer client.Close()
-	reader:=bufio.NewReader(os.Stdin)
-	fmt.Println("Enter book id: ")
-	var bookid int 
-	fmt.Scanln(&bookid)
-	fmt.Println("Enter book title: ")
-	title,_:=reader.ReadString('\n')
-	title=strings.TrimSpace(title)
-	newBook:=&Book{BookID:bookid,Title:title}
-	var msgReply string 
-	var boolReply bool 
-	client.Call("Library.Add",newBook,&msgReply)
-	fmt.Println("Add: ",msgReply)
-	args:=&BookArgs{BookID:bookid}
-	client.Call("Library.CheckAvailability",args,&boolReply)
-	fmt.Println("CheckAvailability: ",boolReply)
-	client.Call("Library.Issue",args,&msgReply)
-	fmt.Println("Issue",msgReply)
-	client.Call("Library.CheckAvailability",args,&boolReply)
-	fmt.Println("CheckAvailability: ",boolReply)
-	client.Call("Library.Return",args,&msgReply)
-	fmt.Println("Return",msgReply)
-	client.Call("Library.CheckAvailability",args,&boolReply)
-	fmt.Println("CheckAvailability: ",boolReply)
+	for{
+		fmt.Println("Enter what to do: ")
+		fmt.Println("1. Add Book")
+		fmt.Println("2.Check Availability Book")
+		fmt.Println("3. Issue Book")
+		fmt.Println("4. Return Book")
+		fmt.Println("5. Exit")
+		var opt int 
+		fmt.Scanln(&opt)
+		var msgReply string 
+		var boolReply bool 
+		var bookid int 
+		reader:=bufio.NewReader(os.Stdin)
+		if opt==1{
+			fmt.Println("Enter book id: ")
+			fmt.Scanln(&bookid)
+			fmt.Println("Enter book title: ")
+			title,_:=reader.ReadString('\n')
+			title=strings.TrimSpace(title)
+			newBook:=&Book{BookID:bookid,Title:title}
+			client.Call("Library.Add",newBook,&msgReply)
+			fmt.Println("Add: ",msgReply)
+		}else if opt==2{
+			fmt.Println("Enter book id: ")
+			fmt.Scanln(&bookid)
+			args:=&BookArgs{BookID:bookid}
+			client.Call("Library.CheckAvailability",args,&boolReply)
+			fmt.Println("CheckAvailability: ",boolReply)
+		}else if opt==3{
+			fmt.Println("Enter book id: ")
+			fmt.Scanln(&bookid)
+			args:=&BookArgs{BookID:bookid}
+			client.Call("Library.Issue",args,&msgReply)
+			fmt.Println("Issue",msgReply)
+		}else if opt==4{
+			fmt.Println("Enter book id: ")
+			fmt.Scanln(&bookid)
+			args:=&BookArgs{BookID:bookid}
+			client.Call("Library.Return",args,&msgReply)
+			fmt.Println("Return",msgReply)
+
+		}else{
+			break
+		}
+	}		
 }
